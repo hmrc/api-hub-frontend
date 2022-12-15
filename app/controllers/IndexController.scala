c3a7b0d5b1ee831f2793636686ac8bc8e5089a41
laurahk <laurahk@users.noreply.github.com>
2022-12-15 12:29:40 +0000

    HIPP-199: remove identity action to skip auth - Laura

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -16,20 +16,19 @@
 
 package controllers
 
-import controllers.actions.IdentifierAction
-import javax.inject.Inject
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.IndexView
 
+import javax.inject.Inject
+
 class IndexController @Inject()(
                                  val controllerComponents: MessagesControllerComponents,
-                                 identify: IdentifierAction,
                                  view: IndexView
                                ) extends FrontendBaseController with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = identify { implicit request =>
+  def onPageLoad: Action[AnyContent] = Action { implicit request =>
     Ok(view())
   }
 }
