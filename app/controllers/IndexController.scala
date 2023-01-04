5679387c3f93078fc27292d90c7be98d0b4a0d6d
PaulCDurham <23658502+PaulCDurham@users.noreply.github.com>
2023-01-04 15:04:33 +0000

    Index controller authenticates and initiates session state

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -16,19 +16,25 @@
 
 package controllers
 
+import controllers.actions.IdentifierAction
+import models.UserAnswers
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.IndexView
 
 import javax.inject.Inject
+import scala.concurrent.ExecutionContext
 
 class IndexController @Inject()(
                                  val controllerComponents: MessagesControllerComponents,
+                                 identify: IdentifierAction,
+                                 sessionRepository: SessionRepository,
                                  view: IndexView
-                               ) extends FrontendBaseController with I18nSupport {
+                               )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = Action { implicit request =>
-    Ok(view())
+  def onPageLoad: Action[AnyContent] = identify.async { implicit request =>
+    sessionRepository.set(UserAnswers(request.userId)).map(_ => Ok(view()))
   }
 }
