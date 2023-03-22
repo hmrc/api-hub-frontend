a5564f2349b4c6cc7b4c5bfa13b8a28911dfb4f5
mike.jackson1 <mike.jackson1@digital.hmrc.gov.uk>
2023-03-22 14:50:01 +0000

    HIPP-440: Upgrade frontend dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  private val bootstrapPlayVersion = "7.13.0"
+  private val bootstrapPlayVersion = "7.15.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "6.6.0-play-28" excludeAll(ExclusionRule("uk.gov.hmrc","url-builder_2.12")),
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.1.0-play-28" excludeAll(ExclusionRule("uk.gov.hmrc","url-builder_2.12")),
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.12.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % bootstrapPlayVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.74.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "1.1.0",
     "uk.gov.hmrc"       %% "http-verbs-play-28"             % "14.9.0",
     "uk.gov.hmrc"       %% "internal-auth-client-play-28"   % "1.4.0"
 
