be5d1847feb8cb6f0e4df3cf3f1a51d436e702b3
mike.jackson1 <mike.jackson1@digital.hmrc.gov.uk>
2023-10-13 16:35:04 +0100

    HIPP-774: Display apis alphabetically

--- app/controllers/HipApisController.scala
+++ app/controllers/HipApisController.scala
@@ -38,7 +38,7 @@ class HipApisController @Inject()(
   def onPageLoad(): Action[AnyContent] = optionallyIdentified.async {
     implicit request =>
       apiHubService.getAllHipApis().map {
-        case apiDetails: Seq[ApiDetail] => Ok(view(request.user, apiDetails))
+        case apiDetails: Seq[ApiDetail] => Ok(view(request.user, apiDetails.sortWith( _.title < _.title)))
         case _ => InternalServerError
       }
   }
