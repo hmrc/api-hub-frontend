bfdfc5177e507ae41bfc9986dfe6ed511d0418c8
Paul Durham <23658502+PaulCDurham@users.noreply.github.com>
2023-10-13 15:02:21 +0100

    HIPP-730: Create application details page (#116)
    * HIPP-730: Create application details page
    
    * Tests done
    
    * Test done
    
    * Tidy up

---
+++ app/viewmodels/application/ApplicationNavItems.scala
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels.application
+
+import models.application.Application
+import play.api.i18n.Messages
+import play.api.mvc.Call
+
+sealed trait ApplicationPage
+
+object ApplicationPages {
+
+  case object DetailsPage extends ApplicationPage
+  case object ApisPage extends ApplicationPage
+  case object EnvironmentsAndCredentialsPage extends ApplicationPage
+  case object ManageTeamMembersPage extends ApplicationPage
+  case object EditApplicationNamePage extends ApplicationPage
+  case object LeaveApplicationPage extends ApplicationPage
+  case object DeleteApplicationPage extends ApplicationPage
+
+}
+
+case class ApplicationNavItem(page: ApplicationPage, title: String, link: Call, isCurrentPage: Boolean)
+
+object ApplicationNavItems {
+
+  import ApplicationPages._
+
+  def apply(application: Application, currentPage: ApplicationPage)(implicit messages: Messages): Seq[ApplicationNavItem] = {
+    Seq(
+      ApplicationNavItem(
+        page = DetailsPage,
+        title = messages("applicationNav.page.applicationDetails"),
+        link = controllers.application.routes.ApplicationDetailsController.onPageLoad(application.id),
+        isCurrentPage = currentPage == DetailsPage
+      ),
+      ApplicationNavItem(
+        page = ApisPage,
+        title = messages("applicationNav.page.applicationApis"),
+        link = controllers.application.routes.ApplicationDetailsController.onPageLoad(application.id),
+        isCurrentPage = currentPage == ApisPage
+      ),
+      ApplicationNavItem(
+        page = EnvironmentsAndCredentialsPage,
+        title = messages("applicationNav.page.environmentsAndCredentials"),
+        link = controllers.application.routes.ApplicationDetailsController.onPageLoad(application.id),
+        isCurrentPage = currentPage == EnvironmentsAndCredentialsPage
+      ),
+      ApplicationNavItem(
+        page = ManageTeamMembersPage,
+        title = messages("applicationNav.page.manageTeamMembers"),
+        link = controllers.application.routes.ApplicationDetailsController.onPageLoad(application.id),
+        isCurrentPage = currentPage == ManageTeamMembersPage
+      ),
+      ApplicationNavItem(
+        page = EditApplicationNamePage,
+        title = messages("applicationNav.page.editApplicationName"),
+        link = controllers.application.routes.ApplicationDetailsController.onPageLoad(application.id),
+        isCurrentPage = currentPage == EditApplicationNamePage
+      ),
+      ApplicationNavItem(
+        page = LeaveApplicationPage,
+        title = messages("applicationNav.page.leaveApplication"),
+        link = controllers.application.routes.ApplicationDetailsController.onPageLoad(application.id),
+        isCurrentPage = currentPage == LeaveApplicationPage
+      ),
+      ApplicationNavItem(
+        page = DeleteApplicationPage,
+        title = messages("applicationNav.page.deleteApplication"),
+        link = controllers.application.routes.ApplicationDetailsController.onPageLoad(application.id),
+        isCurrentPage = currentPage == DeleteApplicationPage
+      )
+    )
+  }
+
+}
---
+++ test/viewmodels/application/ApplicationNavItemsSpec.scala
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels.application
+
+import base.SpecBase
+import controllers.actions.FakeApplication
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
+import play.api.i18n.Messages
+import play.api.test.Helpers._
+import viewmodels.application.ApplicationPages._
+
+class ApplicationNavItemsSpec extends SpecBase with Matchers with TableDrivenPropertyChecks {
+
+  "ApplicationNavItems" - {
+    "must return the correct list of nav items" in {
+      val playApplication = applicationBuilder(None).build()
+
+      running(playApplication) {
+        implicit val implicitMessages: Messages = messages(playApplication)
+
+        val actual = ApplicationNavItems(FakeApplication, DetailsPage)
+        val expected = Seq(
+          ApplicationNavItem(
+            DetailsPage,
+            "Application details",
+            controllers.application.routes.ApplicationDetailsController.onPageLoad(FakeApplication.id),
+            isCurrentPage = true
+          ),
+          ApplicationNavItem(
+            ApisPage,
+            "Application APIs",
+            controllers.application.routes.ApplicationDetailsController.onPageLoad(FakeApplication.id),
+            isCurrentPage = false
+          ),
+          ApplicationNavItem(
+            EnvironmentsAndCredentialsPage,
+            "Environments and credentials",
+            controllers.application.routes.ApplicationDetailsController.onPageLoad(FakeApplication.id),
+            isCurrentPage = false
+          ),
+          ApplicationNavItem(
+            ManageTeamMembersPage,
+            "Manage team members",
+            controllers.application.routes.ApplicationDetailsController.onPageLoad(FakeApplication.id),
+            isCurrentPage = false
+          ),
+          ApplicationNavItem(
+            EditApplicationNamePage,
+            "Edit application name",
+            controllers.application.routes.ApplicationDetailsController.onPageLoad(FakeApplication.id),
+            isCurrentPage = false
+          ),
+          ApplicationNavItem(
+            LeaveApplicationPage,
+            "Leave application",
+            controllers.application.routes.ApplicationDetailsController.onPageLoad(FakeApplication.id),
+            isCurrentPage = false
+          ),
+          ApplicationNavItem(
+            DeleteApplicationPage,
+            "Delete application",
+            controllers.application.routes.ApplicationDetailsController.onPageLoad(FakeApplication.id),
+            isCurrentPage = false
+          )
+        )
+
+        actual mustBe expected
+      }
+    }
+
+    "must select the correct current page" in {
+      val pages = Table(
+        "page",
+        DetailsPage,
+        ApisPage,
+        EnvironmentsAndCredentialsPage,
+        ManageTeamMembersPage,
+        EditApplicationNamePage,
+        LeaveApplicationPage,
+        DeleteApplicationPage
+      )
+
+      val playApplication = applicationBuilder(None).build()
+
+      running(playApplication) {
+        implicit val implicitMessages: Messages = messages(playApplication)
+
+        forAll(pages) {
+          page =>
+            val actual = ApplicationNavItems(FakeApplication, page)
+              .filter(_.isCurrentPage)
+              .map(_.page)
+
+            actual mustBe Seq(page)
+        }
+      }
+    }
+  }
+
+}
