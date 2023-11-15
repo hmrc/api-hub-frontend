8085f20f52652928b48bfd953cc6ecb7f59d1841
PaulCDurham <23658502+PaulCDurham@users.noreply.github.com>
2023-11-15 14:21:17 +0000

    Viewmodel test

---
+++ test/viewmodels/admin/AdminNavItemsSpec.scala
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels.admin
+
+import base.SpecBase
+import controllers.actions.{FakeApprover, FakeSupporter}
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
+import play.api.i18n.Messages
+import play.api.test.Helpers.running
+import viewmodels.SideNavItem
+import viewmodels.admin.AdminSideNavPages.{AccessRequestsPage, ManageApplicationsPage}
+
+class AdminNavItemsSpec extends SpecBase with Matchers with TableDrivenPropertyChecks {
+
+  import AdminNavItemsSpec._
+
+  "class AdminNavItems" - {
+    "must return the correct list of nav items for support" in {
+      val playApplication = applicationBuilder(None).build()
+
+      running(playApplication) {
+        implicit val implicitMessages: Messages = messages(playApplication)
+        val actual = AdminNavItems(FakeSupporter, AccessRequestsPage)
+        val expected = Seq(manageApplicationsNavItem(), accessRequestsNavItem())
+
+        actual mustBe expected
+      }
+    }
+
+    "must return the correct list of nav items for an approver" in {
+      val playApplication = applicationBuilder(None).build()
+
+      running(playApplication) {
+        implicit val implicitMessages: Messages = messages(playApplication)
+        val actual = AdminNavItems(FakeApprover, AccessRequestsPage)
+        val expected = Seq(accessRequestsNavItem())
+
+        actual mustBe expected
+      }
+    }
+
+    "must select the correct current page" in {
+      val pages = Table(
+        "Page",
+        ManageApplicationsPage,
+        AccessRequestsPage
+      )
+
+      val playApplication = applicationBuilder(None).build()
+
+      running(playApplication) {
+        implicit val implicitMessages: Messages = messages(playApplication)
+
+        forAll(pages) {
+          page =>
+            val actual = AdminNavItems(FakeSupporter, page)
+              .filter(_.isCurrentPage)
+              .map(_.page)
+
+            actual mustBe Seq(page)
+        }
+      }
+
+    }
+  }
+
+}
+
+object AdminNavItemsSpec {
+
+  private def accessRequestsNavItem(): SideNavItem = {
+    SideNavItem(
+      page = AccessRequestsPage,
+      title = "API requests",
+      link = controllers.admin.routes.AccessRequestsController.onPageLoad(),
+      isCurrentPage = true
+    )
+  }
+
+  private def manageApplicationsNavItem(): SideNavItem = {
+    SideNavItem(
+      page = ManageApplicationsPage,
+      title = "Manage applications",
+      link = controllers.admin.routes.AccessRequestsController.onPageLoad(),
+      isCurrentPage = false
+    )
+  }
+
+}
