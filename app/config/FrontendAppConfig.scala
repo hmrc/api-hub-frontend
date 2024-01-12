454aa91b45e27ee28c819d1683cee6b62371fb66
Paul Durham <23658502+PaulCDurham@users.noreply.github.com>
2024-01-12 11:15:29 +0000

    HIPP-937: Add the ability to shutter the api-hub frontend (#164)

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -55,4 +55,7 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
   val countdown: Int = configuration.get[Int]("timeout-dialog.countdown")
 
   val cacheTtl: Int = configuration.get[Int]("mongodb.timeToLiveInSeconds")
+
+  val shutterMessage: String = configuration.get[String]("shutter-message")
+
 }
