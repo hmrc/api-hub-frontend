a4b5dd6e41f13f56fc1cdcdc95c6bcc14c3bbc83
PaulCDurham <23658502+PaulCDurham@users.noreply.github.com>
2024-01-15 15:52:05 +0000

    Upgrade to Play 3.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,32 +3,32 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  private val bootstrapPlayVersion = "7.22.0"
-  private val hmrcMongoVersion = "1.3.0"
+  private val bootstrapPlayVersion = "8.4.0"
+  private val hmrcMongoVersion = "1.7.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.23.0-play-28" excludeAll(ExclusionRule("uk.gov.hmrc","url-builder_2.12")),
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % bootstrapPlayVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "internal-auth-client-play-28"   % "1.6.0",
-    "uk.gov.hmrc"       %% "crypto-json-play-28"            % "7.3.0"
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "8.3.0" excludeAll(ExclusionRule("uk.gov.hmrc","url-builder_2.12")),
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "2.0.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"     % bootstrapPlayVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"             % hmrcMongoVersion,
+    "uk.gov.hmrc"       %% "internal-auth-client-play-30"   % "1.9.0",
+    "uk.gov.hmrc"       %% "crypto-json-play-30"            % "7.6.0"
   )
 
   val test = Seq(
-    "org.scalatest"           %% "scalatest"               % "3.2.10",
-    "org.scalatestplus"       %% "scalacheck-1-15"         % "3.2.10.0",
-    "org.scalatestplus"       %% "mockito-3-4"             % "3.2.10.0",
-    "org.scalatestplus.play"  %% "scalatestplus-play"      % "5.1.0",
+    "org.scalatest"           %% "scalatest"               % "3.2.17",
+    "org.scalatestplus"       %% "scalacheck-1-17"         % "3.2.17.0",
+    "org.scalatestplus"       %% "mockito-5-8"             % "3.2.17.0",
+    "org.scalatestplus.play"  %% "scalatestplus-play"      % "7.0.1",
     "org.pegdown"             %  "pegdown"                 % "1.6.0",
-    "org.jsoup"               %  "jsoup"                   % "1.14.3",
-    "com.typesafe.play"       %% "play-test"               % PlayVersion.current,
-    "org.mockito"             %% "mockito-scala"           % "1.16.42",
-    "org.scalacheck"          %% "scalacheck"              % "1.15.4",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28" % hmrcMongoVersion,
+    "org.jsoup"               %  "jsoup"                   % "1.17.2",
+    "org.playframework"       %% "play-test"               % PlayVersion.current,
+    "org.mockito"             %% "mockito-scala"           % "1.17.30",
+    "org.scalacheck"          %% "scalacheck"              % "1.17.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
     "com.vladsch.flexmark"    %  "flexmark-all"            % "0.62.2",
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"  % bootstrapPlayVersion,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapPlayVersion,
     "nu.validator"            %  "validator"               % "20.7.2"
   ).map(_ % "test, it")
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,19 +4,19 @@ resolvers += Resolver.url("HMRC-open-artefacts-ivy", url("https://open.artefacts
 
 resolvers += "Typesafe Releases" at "https://repo.typesafe.com/typesafe/releases/"
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.15.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.19.0")
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.2.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.4.0")
 
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.8.20")
+addSbtPlugin("org.playframework" % "sbt-plugin" % "3.0.1")
 
-addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")
+addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0" exclude("org.scala-lang.modules", "scala-xml_2.12"))
 
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.9.0")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.0.9")
 
 addSbtPlugin("net.ground5hark.sbt" % "sbt-concat" % "0.2.0")
 
-addSbtPlugin("com.typesafe.sbt" % "sbt-uglify" % "2.0.0")
+//addSbtPlugin("com.github.sbt" % "sbt-uglify" % "3.0.0")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-digest" % "1.1.4")
 
