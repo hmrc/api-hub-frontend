749668865a4e9bc590cb6ed02ee10b09c280a798
rdawson-hmrc <161580719+rdawson-hmrc@users.noreply.github.com>
2024-03-07 14:44:33 +0000

    codemirror

--- app/assets/oaseditors/codemirror/init.js
+++ app/assets/oaseditors/codemirror/init.js
@@ -2,8 +2,3 @@ const initialState = cm6.createEditorState(oasYaml);
 const view = cm6.createEditorView(initialState, document.getElementById("codemirror-editor"));
 
 // to get editor contents: view.state.doc.toString()
-debugger
-function highlightLine(lineNumber) {
-
-}
-highlightLine(10);
