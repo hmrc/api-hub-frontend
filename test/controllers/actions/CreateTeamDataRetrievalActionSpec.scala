c8fe6df497f87d2b44bec1b8ecc9dc4294029912
rdawson-hmrc <161580719+rdawson-hmrc@users.noreply.github.com>
2024-03-27 14:11:58 +0000

    HIPP-1084 - adding new User Answers repo for Create Team

---
+++ test/controllers/actions/CreateTeamDataRetrievalActionSpec.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.actions
+
+import base.SpecBase
+import models.UserAnswers
+import models.requests.{IdentifierRequest, OptionalDataRequest}
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.test.FakeRequest
+import repositories.CreateTeamSessionRepository
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class CreateTeamDataRetrievalActionSpec extends SpecBase with MockitoSugar {
+
+  class Harness(sessionRepository: CreateTeamSessionRepository) extends CreateTeamDataRetrievalActionImpl(sessionRepository) {
+    def callTransform[A](request: IdentifierRequest[A]): Future[OptionalDataRequest[A]] = transform(request)
+  }
+
+  "Data Retrieval Action" - {
+
+    "when there is no data in the cache" - {
+
+      "must set userAnswers to 'None' in the request" in {
+
+        val sessionRepository = mock[CreateTeamSessionRepository]
+        when(sessionRepository.get("id")) thenReturn Future(None)
+        val action = new Harness(sessionRepository)
+
+        val result = action.callTransform(IdentifierRequest(FakeRequest(), FakeUser)).futureValue
+
+        result.userAnswers must not be defined
+      }
+    }
+
+    "when there is data in the cache" - {
+
+      "must build a userAnswers object and add it to the request" in {
+
+        val sessionRepository = mock[CreateTeamSessionRepository]
+        when(sessionRepository.get("id")) thenReturn Future(Some(UserAnswers("id")))
+        val action = new Harness(sessionRepository)
+
+        val result = action.callTransform(IdentifierRequest(FakeRequest(), FakeUser)).futureValue
+
+        result.userAnswers mustBe defined
+      }
+    }
+  }
+
+}
