e9b3e905d278c8c96e1a49b195194e5c6ee4e466
Paul Durham <23658502+PaulCDurham@users.noreply.github.com>
2024-03-27 15:04:16 +0000

    HIPP-1060: Upgrade to play-frontend-hmrc v9.0.0 (#189)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,26 +3,27 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  private val bootstrapPlayVersion = "8.4.0"
-  private val hmrcMongoVersion = "1.7.0"
+  private val bootstrapPlayVersion = "8.5.0"
+  private val hmrcMongoVersion = "1.8.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "8.3.0" excludeAll(ExclusionRule("uk.gov.hmrc","url-builder_2.12")),
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"     % "9.3.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "2.0.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"     % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"             % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "internal-auth-client-play-30"   % "1.9.0",
+    "uk.gov.hmrc"       %% "internal-auth-client-play-30"   % "2.0.0",
     "uk.gov.hmrc"       %% "crypto-json-play-30"            % "7.6.0"
   )
 
   val test = Seq(
-    "org.scalatestplus"       %% "scalacheck-1-17"         % "3.2.17.0",
+    "org.scalatestplus"       %% "scalacheck-1-17"         % "3.2.18.0",
     "org.mockito"             %% "mockito-scala"           % "1.17.30",
     "org.scalacheck"          %% "scalacheck"              % "1.17.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapPlayVersion,
-    "nu.validator"            %  "validator"               % "20.7.2"
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30" % hmrcMongoVersion excludeAll(ExclusionRule("com.vladsch.flexmark","flexmark-all")),
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapPlayVersion excludeAll(ExclusionRule("com.vladsch.flexmark","flexmark-all")),
+    "nu.validator"            %  "validator"               % "20.7.2",
+    "com.vladsch.flexmark"    %  "flexmark-all"            % "0.62.2"   // Increasing from this version breaks nu.validator:validator
   ).map(_ % Test)
 
   val it = Seq.empty
