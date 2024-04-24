7c7171b8b96d902e920e7a303ccc7b6e530c02e7
rdawson-hmrc <161580719+rdawson-hmrc@users.noreply.github.com>
2024-04-24 15:53:56 +0100

    HIPP-1140 - minor fixes

--- app/assets/javascripts/hipApis.js
+++ app/assets/javascripts/hipApis.js
@@ -1,22 +1,67 @@
-const view = (() => {
-    return {
-        onStatusFilterChange: function(handler) {
-            document.querySelectorAll('#statusFilters .govuk-checkboxes__input').forEach(elCheckbox => {
-                elCheckbox.addEventListener('change', handler);
-            });
-        },
-        getStatusFilters: function() {
-            return Array.from(document.querySelectorAll('#statusFilters .govuk-checkboxes__input')).reduce((acc, elCheckbox) => {
-                acc[elCheckbox.value] = elCheckbox.checked;
-                return acc;
-            }, {});
-        },
+document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
 
-    };
-})();
+    const view = (() => {
+        const statusFilterEls = Array.from(document.querySelectorAll('#statusFilters .govuk-checkboxes__input')),
+            apiDetailPanelEls = Array.from(document.querySelectorAll('#apiList .api-panel')),
+            elSearchResultsSize = document.getElementById('searchResultsSize');
 
-document.addEventListener('DOMContentLoaded', function(event) {
-    view.onStatusFilterChange(function(status, checked) {
-        console.log(view.getStatusFilters());
+        return {
+            onFiltersChanged(handler) {
+                statusFilterEls.forEach(elCheckbox => {
+                    elCheckbox.addEventListener('change', handler);
+                });
+            },
+            getStatusFilterValues() {
+                return statusFilterEls.filter(el => el.checked).map(el => el.value);
+            },
+            getApiDetailPanels() {
+                return apiDetailPanelEls;
+            },
+            updateApiDetailsPanels(model) {
+                let visibleCount = 0;
+                model.forEach(apiDetail => {
+                    apiDetail.el.style.display = apiDetail.visible ? 'block' : 'none';
+                    if (apiDetail.visible) {
+                        visibleCount++;
+                    }
+                });
+                elSearchResultsSize.textContent = visibleCount.toString();
+            },
+        };
+    })();
+
+    function buildFilterFunctions() {
+        function buildApiStatusFilterFunction() {
+            const selectedStatuses = new Set(view.getStatusFilterValues());
+            return data => selectedStatuses.has(data.apiStatus);
+        }
+        // add new filters here...
+
+        return [buildApiStatusFilterFunction()];
+    }
+
+    const model = view.getApiDetailPanels().map(el => ({
+        data: {
+            apiStatus: el.dataset['apistatus']
+            // add other properties that we want to filter on here...
+        },
+        el,
+        visible: true
+    }));
+
+    function applyFilters() {
+        const filterFns = buildFilterFunctions();
+        model.forEach(apiDetail => {
+            apiDetail.visible = filterFns.every(fn => fn(apiDetail.data));
+        });
+        view.updateApiDetailsPanels(model);
+    }
+
+    view.onFiltersChanged(() => {
+        applyFilters();
     });
-})
+
+    applyFilters();
+
+});
