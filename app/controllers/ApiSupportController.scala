d48893f5f44ed6ac686822001169b433a763d552
Paul Durham <23658502+PaulCDurham@users.noreply.github.com>
2024-04-30 12:00:02 +0100

    Add support view of APIs (#216)

---
+++ app/controllers/ApiSupportController.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import com.google.inject.{Inject, Singleton}
+import controllers.actions.{AuthorisedSupportAction, IdentifierAction}
+import models.api.ApiDetail
+import play.api.i18n.I18nSupport
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Request}
+import services.ApiHubService
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+
+import scala.concurrent.ExecutionContext
+
+@Singleton
+class ApiSupportController @Inject()(
+  override val controllerComponents: MessagesControllerComponents,
+  identify: IdentifierAction,
+  isSupport: AuthorisedSupportAction,
+  apiHubService: ApiHubService
+)(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  def onPageLoad(id: String): Action[AnyContent] = (identify andThen isSupport).async {
+    implicit request =>
+      apiHubService
+        .getApiDetail(id)
+        .map {
+          case Some(apiDetail) => Ok(Json.toJson(buildApiDetail(apiDetail)))
+          case _ => NotFound
+        }
+  }
+
+  private def buildApiDetail(apiDetail: ApiDetail)(implicit request: Request[_]): ApiDetail = {
+    apiDetail.copy(
+      openApiSpecification = controllers.routes.OasRedocController.getOas(apiDetail.id).absoluteURL()
+    )
+  }
+
+}
