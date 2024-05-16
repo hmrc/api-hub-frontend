7bd417c6f48283650da3e82b459d9d4673b74a3d
rdawson-hmrc <161580719+rdawson-hmrc@users.noreply.github.com>
2024-05-16 15:57:45 +0100

    HIPP-1161 - using new Domains helper methods

--- app/controllers/HipApisController.scala
+++ app/controllers/HipApisController.scala
@@ -40,7 +40,7 @@ class HipApisController @Inject()(
   def onPageLoad(): Action[AnyContent] = optionallyIdentified.async {
     implicit request =>
       apiHubService.getAllHipApis().map {
-        case apiDetails: Seq[ApiDetail] => Ok(view(request.user, apiDetails.sortWith( _.title.toUpperCase < _.title.toUpperCase), domains.domains))
+        case apiDetails: Seq[ApiDetail] => Ok(view(request.user, apiDetails.sortWith( _.title.toUpperCase < _.title.toUpperCase), domains))
         case _ => InternalServerError
       }
   }
