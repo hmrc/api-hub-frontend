95f4ae9e947be0dfc2e2cee0228c76fcc294b8ea
rdawson-hmrc <161580719+rdawson-hmrc@users.noreply.github.com>
2024-05-17 16:02:16 +0100

    HIPP-1161 - more js tests

--- jsTest/tests/hipApis.spec.js
+++ jsTest/tests/hipApis.spec.js
@@ -13,6 +13,26 @@ describe('hipApis', () => {
                 <input class="govuk-checkboxes__input" type="checkbox" value="LIVE" checked>
                 <input class="govuk-checkboxes__input" type="checkbox" value="DEPRECATED">                
             </div>
+            <div id="domainFilters">
+                <div><input class="domainFilter" type="checkbox" value="d1"></div>
+                <div class="subdomainCheckboxes" data-domain="d1">
+                    <div><input class="subDomainFilter" type="checkbox" value="d1s1" data-domain="d1"></div>
+                    <div><input class="subDomainFilter" type="checkbox" value="d1s2" data-domain="d1"></div>
+                    <div><input class="subDomainFilter" type="checkbox" value="d1s3" data-domain="d1"></div>
+                </div>
+                <div><input class="domainFilter" type="checkbox" value="d2"></div>
+                <div class="subdomainCheckboxes" data-domain="d2">
+                    <div><input class="subDomainFilter" type="checkbox" value="d2s1" data-domain="d2"></div>
+                    <div><input class="subDomainFilter" type="checkbox" value="d2s2" data-domain="d2"></div>
+                    <div><input class="subDomainFilter" type="checkbox" value="d2s3" data-domain="d2"></div>
+                </div>
+                <div><input class="domainFilter" type="checkbox" value="d3"></div>
+                <div class="subdomainCheckboxes" data-domain="d3">
+                    <div><input class="subDomainFilter" type="checkbox" value="d3s1" data-domain="d3"></div>
+                    <div><input class="subDomainFilter" type="checkbox" value="d3s2" data-domain="d3"></div>
+                    <div><input class="subDomainFilter" type="checkbox" value="d3s3" data-domain="d3"></div>
+                </div>
+            </div>            
             <div id="apiList"></div>
             <div id="searchResultsSize"></div>
             <div id="pagination"></div>
@@ -28,26 +48,49 @@ describe('hipApis', () => {
 
     function buildApiPanelsByCount(count) {
         const panels = [],
-            statuses = ['ALPHA', 'BETA', 'LIVE', 'DEPRECATED'];
+            statuses = ['ALPHA', 'BETA', 'LIVE', 'DEPRECATED'],
+            domainValues = [
+                ['d1', 'd1s1'],
+                ['d1', 'd1s2'],
+                ['d1', 'd1s3'],
+                ['d2', 'd2s1'],
+                ['d3', ''],
+                ['', 'd3s1'],
+                ['dx', 'dxs1'],
+                ['', ''],
+            ];
         let i= 0;
         while (i < count) {
-            panels.push({apiStatus: statuses[i % statuses.length]});
+            const apiStatus = statuses[i % statuses.length],
+                [domain, subdomain] = domainValues[i % domainValues.length];
+            panels.push({apiStatus, domain, subdomain})
             i++;
         }
         buildApiPanels(...panels);
     }
     function buildApiPanels(...panels) {
         document.getElementById('apiList').innerHTML = panels.map((panel, i) => {
-            return `<div class="api-panel" data-apistatus="${panel.apiStatus}" data-index="${i}"></div>`;
+            return `<div class="api-panel" data-apistatus="${panel.apiStatus}" data-domain="${panel.domain}" data-subdomain="${panel.subdomain}" data-index="${i}"></div>`;
         }).join('');
     }
     function getVisiblePanelData() {
         return Array.from(document.querySelectorAll('.api-panel'))
             .filter(el => el.style.display === 'block')
-            .map(el => ({apiStatus: el.dataset['apistatus'], index: parseInt(el.dataset['index'])}));
+            .map(el => ({
+                apiStatus: el.dataset['apistatus'],
+                index: parseInt(el.dataset['index']),
+                domain: el.dataset['domain'],
+                subdomain: el.dataset['subdomain']
+            }));
     }
     function getResultCount() {
         return parseInt(document.getElementById('searchResultsSize').textContent);
+    }
+    function clickDomainFilter(value) {
+        document.querySelector(`input[value="${value}"].domainFilter`).click();
+    }
+    function clickSubdomainFilter(value) {
+        document.querySelector(`input[value="${value}"].subDomainFilter`).click();
     }
     function clickStatusFilter(value) {
         document.querySelector(`#statusFilters input[value="${value}"]`).click();
@@ -55,13 +98,16 @@ describe('hipApis', () => {
     function clickPageNumber(pageNumber) {
         document.querySelector(`#pagination .govuk-pagination__link[data-page="${pageNumber}"]`).click();
     }
+    function indexAndStatus(panel) {
+        return {apiStatus: panel.apiStatus, index: panel.index};
+    }
 
     it("when page initially displayed then only panels with selected statuses are visible",  () => {
         buildApiPanels({apiStatus: 'ALPHA'}, {apiStatus: 'BETA'}, {apiStatus: 'LIVE'}, {apiStatus: 'DEPRECATED'});
 
         onPageShow();
 
-        expect(getVisiblePanelData()).toEqual([{apiStatus: 'ALPHA', index: 0}, {apiStatus: 'BETA', index: 1}, {apiStatus: 'LIVE', index: 2}]);
+        expect(getVisiblePanelData().map(indexAndStatus)).toEqual([{apiStatus: 'ALPHA', index: 0}, {apiStatus: 'BETA', index: 1}, {apiStatus: 'LIVE', index: 2}]);
         expect(getResultCount()).toBe(3);
     });
 
@@ -71,7 +117,7 @@ describe('hipApis', () => {
         onPageShow();
         clickStatusFilter('BETA');
 
-        expect(getVisiblePanelData()).toEqual([{apiStatus: 'ALPHA', index: 0}, {apiStatus: 'LIVE', index: 2}]);
+        expect(getVisiblePanelData().map(indexAndStatus)).toEqual([{apiStatus: 'ALPHA', index: 0}, {apiStatus: 'LIVE', index: 2}]);
         expect(getResultCount()).toBe(2);
     });
 
@@ -81,7 +127,7 @@ describe('hipApis', () => {
         onPageShow();
         clickStatusFilter('DEPRECATED');
 
-        expect(getVisiblePanelData()).toEqual([{apiStatus: 'ALPHA', index: 0}, {apiStatus: 'BETA', index: 1}, {apiStatus: 'LIVE', index: 2}, {apiStatus: 'DEPRECATED', index: 3}]);
+        expect(getVisiblePanelData().map(indexAndStatus)).toEqual([{apiStatus: 'ALPHA', index: 0}, {apiStatus: 'BETA', index: 1}, {apiStatus: 'LIVE', index: 2}, {apiStatus: 'DEPRECATED', index: 3}]);
         expect(getResultCount()).toBe(4);
     });
 
@@ -116,6 +162,58 @@ describe('hipApis', () => {
         expect(getVisiblePanelData().map(p => p.index)).toEqual(
             [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
         );
+    });
+
+    it("when domain filter is applied then correct panels are shown",  () => {
+        buildApiPanelsByCount(100);
+        onPageShow();
+
+        clickDomainFilter('d1');
+
+        expect(getVisiblePanelData()).toEqual([
+            { apiStatus: 'ALPHA', index: 0, domain: 'd1', subdomain: 'd1s1' },
+            { apiStatus: 'BETA', index: 1, domain: 'd1', subdomain: 'd1s2' },
+            { apiStatus: 'LIVE', index: 2, domain: 'd1', subdomain: 'd1s3' },
+            { apiStatus: 'ALPHA', index: 8, domain: 'd1', subdomain: 'd1s1' },
+            { apiStatus: 'BETA', index: 9, domain: 'd1', subdomain: 'd1s2' },
+            { apiStatus: 'LIVE', index: 10, domain: 'd1', subdomain: 'd1s3' },
+            { apiStatus: 'ALPHA', index: 16, domain: 'd1', subdomain: 'd1s1' },
+            { apiStatus: 'BETA', index: 17, domain: 'd1', subdomain: 'd1s2' },
+            { apiStatus: 'LIVE', index: 18, domain: 'd1', subdomain: 'd1s3' },
+            { apiStatus: 'ALPHA', index: 24, domain: 'd1', subdomain: 'd1s1' },
+            { apiStatus: 'BETA', index: 25, domain: 'd1', subdomain: 'd1s2' },
+            { apiStatus: 'LIVE', index: 26, domain: 'd1', subdomain: 'd1s3' },
+            { apiStatus: 'ALPHA', index: 32, domain: 'd1', subdomain: 'd1s1' },
+            { apiStatus: 'BETA', index: 33, domain: 'd1', subdomain: 'd1s2' },
+            { apiStatus: 'LIVE', index: 34, domain: 'd1', subdomain: 'd1s3' }
+        ]);
+    });
+
+    it("when multiple domain filters are applied then correct panels are shown",  () => {
+        buildApiPanelsByCount(100);
+        onPageShow();
+
+        clickDomainFilter('d1');
+        clickSubdomainFilter('d1s1');
+        clickDomainFilter('d2');
+
+        expect(getVisiblePanelData()).toEqual([
+            { apiStatus: 'BETA', index: 1, domain: 'd1', subdomain: 'd1s2' },
+            { apiStatus: 'LIVE', index: 2, domain: 'd1', subdomain: 'd1s3' },
+            { apiStatus: 'BETA', index: 9, domain: 'd1', subdomain: 'd1s2' },
+            { apiStatus: 'LIVE', index: 10, domain: 'd1', subdomain: 'd1s3' },
+            { apiStatus: 'BETA', index: 17, domain: 'd1', subdomain: 'd1s2' },
+            { apiStatus: 'LIVE', index: 18, domain: 'd1', subdomain: 'd1s3' },
+            { apiStatus: 'BETA', index: 25, domain: 'd1', subdomain: 'd1s2' },
+            { apiStatus: 'LIVE', index: 26, domain: 'd1', subdomain: 'd1s3' },
+            { apiStatus: 'BETA', index: 33, domain: 'd1', subdomain: 'd1s2' },
+            { apiStatus: 'LIVE', index: 34, domain: 'd1', subdomain: 'd1s3' },
+            { apiStatus: 'BETA', index: 41, domain: 'd1', subdomain: 'd1s2' },
+            { apiStatus: 'LIVE', index: 42, domain: 'd1', subdomain: 'd1s3' },
+            { apiStatus: 'BETA', index: 49, domain: 'd1', subdomain: 'd1s2' },
+            { apiStatus: 'LIVE', index: 50, domain: 'd1', subdomain: 'd1s3' },
+            { apiStatus: 'BETA', index: 57, domain: 'd1', subdomain: 'd1s2' }
+        ]);
     });
 
 });
