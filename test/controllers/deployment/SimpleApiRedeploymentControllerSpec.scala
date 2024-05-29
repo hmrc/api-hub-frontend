e3229c6a13c012ab3b8c2fbb80edefb9435ed397
PaulCDurham <23658502+PaulCDurham@users.noreply.github.com>
2024-05-29 14:21:13 +0100

    More tests

--- test/controllers/deployment/SimpleApiRedeploymentControllerSpec.scala
+++ test/controllers/deployment/SimpleApiRedeploymentControllerSpec.scala
@@ -1,24 +1,41 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.deployment
 
 import base.SpecBase
 import connectors.ApplicationsConnector
-import controllers.actions.{FakeApiDetail, FakeUser}
+import controllers.actions.{ApiAuthActionProvider, FakeApiAuthActions, FakeApiDetail, FakeUser}
 import controllers.deployment.SimpleApiRedeploymentController.RedeploymentRequestFormProvider
-import models.application.TeamMember
-import models.deployment.{RedeploymentRequest, SuccessfulDeploymentsResponse}
-import models.team.Team
+import models.deployment.{Error, FailuresResponse, InvalidOasResponse, RedeploymentRequest, SuccessfulDeploymentsResponse}
+import models.user.UserModel
 import org.mockito.{ArgumentMatchersSugar, MockitoSugar}
 import org.scalatest.OptionValues
 import org.scalatest.matchers.must.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
+import play.api.data.Form
 import play.api.inject.bind
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import play.api.{Application => PlayApplication}
 import services.ApiHubService
 import utils.HtmlValidation
-import views.html.deployment.{DeploymentSuccessView, SimpleApiRedeploymentView}
+import views.html.deployment.{DeploymentFailureView, DeploymentSuccessView, SimpleApiRedeploymentView}
 
-import java.time.LocalDateTime
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class SimpleApiRedeploymentControllerSpec
@@ -26,7 +43,9 @@ class SimpleApiRedeploymentControllerSpec
     with Matchers
     with MockitoSugar
     with ArgumentMatchersSugar
-    with HtmlValidation {
+    with HtmlValidation
+    with TableDrivenPropertyChecks
+    with FakeApiAuthActions {
 
   import SimpleApiRedeploymentControllerSpec._
 
@@ -34,7 +53,7 @@ class SimpleApiRedeploymentControllerSpec
     "must return 200 Ok and the correct view" in {
       val fixture = buildFixture()
 
-      mockApiAndTeam(fixture)
+      when(fixture.apiAuthActionProvider.apply(any)(any)).thenReturn(successfulApiAuthAction(FakeApiDetail))
 
       running(fixture.playApplication) {
         val request = FakeRequest(controllers.deployment.routes.SimpleApiRedeploymentController.onPageLoad(FakeApiDetail.id))
@@ -59,8 +78,7 @@ class SimpleApiRedeploymentControllerSpec
         uri = "test-uri"
       )
 
-      mockApiAndTeam(fixture)
-
+      when(fixture.apiAuthActionProvider.apply(any)(any)).thenReturn(successfulApiAuthAction(FakeApiDetail))
       when(fixture.applicationsConnector.updateDeployment(any, any)(any)).thenReturn(Future.successful(Some(response)))
 
       running(fixture.playApplication) {
@@ -76,29 +94,83 @@ class SimpleApiRedeploymentControllerSpec
         verify(fixture.applicationsConnector).updateDeployment(eqTo(FakeApiDetail.publisherReference), eqTo(redeploymentRequest))(any)
       }
     }
+
+    "must respond with 400 Bad Request and a failure view response when failure returned by APIM" in {
+      val fixture = buildFixture()
+
+      val response = InvalidOasResponse(
+        failure = FailuresResponse(
+          code = "test-code",
+          reason = "test-reason",
+          errors = Some(Seq(Error(`type` = "test-type", message = "test-message")))
+        )
+      )
+
+      when(fixture.apiAuthActionProvider.apply(any)(any)).thenReturn(successfulApiAuthAction(FakeApiDetail))
+      when(fixture.applicationsConnector.updateDeployment(any, any)(any)).thenReturn(Future.successful(Some(response)))
+
+      running(fixture.playApplication) {
+        val request = FakeRequest(controllers.deployment.routes.SimpleApiRedeploymentController.onSubmit(FakeApiDetail.id))
+          .withFormUrlEncodedBody(validForm: _*)
+        val result = route(fixture.playApplication, request).value
+        val view = fixture.playApplication.injector.instanceOf[DeploymentFailureView]
+        val returnUrl = controllers.deployment.routes.SimpleApiRedeploymentController.onPageLoad(FakeApiDetail.id).url
+
+        status(result) mustBe BAD_REQUEST
+        contentAsString(result) mustBe view(FakeUser, response.failure, returnUrl)(request, messages(fixture.playApplication)).toString()
+        contentAsString(result) must validateAsHtml
+      }
+    }
+
+    "must return 400 Bad Request and errors when invalid data is submitted" in {
+      val fixture = buildFixture()
+
+      val fieldNames = Table(
+        "Field name",
+        "description",
+        "oas",
+        "status"
+      )
+
+      when(fixture.apiAuthActionProvider.apply(any)(any)).thenReturn(successfulApiAuthAction(FakeApiDetail))
+
+      running(fixture.playApplication) {
+        forAll(fieldNames){(fieldName: String) =>
+          val request = FakeRequest(controllers.deployment.routes.SimpleApiRedeploymentController.onSubmit(FakeApiDetail.id))
+            .withFormUrlEncodedBody(invalidForm(fieldName): _*)
+          val result = route(fixture.playApplication, request).value
+          val boundForm = bindForm(form, invalidForm(fieldName))
+
+          val view = fixture.playApplication.injector.instanceOf[SimpleApiRedeploymentView]
+
+          status(result) mustBe BAD_REQUEST
+          contentAsString(result) mustBe view(boundForm, FakeApiDetail, FakeUser)(request, messages(fixture.playApplication)).toString()
+          contentAsString(result) must validateAsHtml
+        }
+      }
+    }
   }
 
   private case class Fixture(
     playApplication: PlayApplication,
     apiHubService: ApiHubService,
-    applicationsConnector: ApplicationsConnector
+    applicationsConnector: ApplicationsConnector,
+    apiAuthActionProvider: ApiAuthActionProvider
   )
 
-  private def buildFixture(): Fixture = {
+  private def buildFixture(user: UserModel = FakeUser): Fixture = {
     val apiHubService = mock[ApiHubService]
     val applicationsConnector = mock[ApplicationsConnector]
-    val playApplication = applicationBuilder()
+    val apiAuthActionProvider = mock[ApiAuthActionProvider]
+
+    val playApplication = applicationBuilder(user = user)
       .overrides(
         bind[ApiHubService].toInstance(apiHubService),
-        bind[ApplicationsConnector].toInstance(applicationsConnector)
+        bind[ApplicationsConnector].toInstance(applicationsConnector),
+        bind[ApiAuthActionProvider].toInstance(apiAuthActionProvider)
       )
       .build()
-    Fixture(playApplication, apiHubService, applicationsConnector)
-  }
-
-  private def mockApiAndTeam(fixture: Fixture): Unit = {
-    when(fixture.apiHubService.getApiDetail(eqTo(FakeApiDetail.id))(any)).thenReturn(Future.successful(Some(FakeApiDetail)))
-    when(fixture.apiHubService.findTeams(eqTo(FakeUser.email))(any)).thenReturn(Future.successful(userTeams))
+    Fixture(playApplication, apiHubService, applicationsConnector, apiAuthActionProvider)
   }
 
 }
@@ -106,17 +178,6 @@ class SimpleApiRedeploymentControllerSpec
 object SimpleApiRedeploymentControllerSpec extends OptionValues {
 
   private val form = new RedeploymentRequestFormProvider()()
-
-  private lazy val owningTeam = Team(
-    id = FakeApiDetail.teamId.value,
-    name = "test-team-name",
-    created = LocalDateTime.now(),
-    teamMembers = Seq(
-      TeamMember(FakeUser.email.value)
-    )
-  )
-
-  private lazy val userTeams: Seq[Team] = Seq(owningTeam)
 
   private val redeploymentRequest = RedeploymentRequest(
     description = "test-description",
@@ -129,5 +190,12 @@ object SimpleApiRedeploymentControllerSpec extends OptionValues {
     "oas" -> redeploymentRequest.oas,
     "status" -> redeploymentRequest.status
   )
+
+  private def invalidForm(missingField: String): Seq[(String, String)] =
+    validForm.filterNot(_._1.equalsIgnoreCase(missingField)) :+ (missingField, "")
+
+  private def bindForm(form: Form[_], values: Seq[(String, String)]): Form[_] = {
+    form.bind(values.toMap)
+  }
 
 }
