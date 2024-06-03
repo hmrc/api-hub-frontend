251f90571b6fe28df8eb29f2d1547b626af3c8a2
Rob Dawson <161580719+rdawson-hmrc@users.noreply.github.com>
2024-06-03 15:35:48 +0100

    HIPP-1278 - move status filter (#249)

--- app/assets/javascripts/hipApisStatusFilters.js
+++ app/assets/javascripts/hipApisStatusFilters.js
@@ -1,5 +1,7 @@
 export function buildStatusFilters() {
-    const statusFilterEls = Array.from(document.querySelectorAll('#statusFilters .govuk-checkboxes__input'));
+    const statusFilterEls = Array.from(document.querySelectorAll('#statusFilters .govuk-checkboxes__input')),
+        elStatusFilterCount = document.getElementById('statusFilterCount'),
+        elViewStatusFilters = document.getElementById('viewStatusFilters');
 
     let onFiltersChangedHandler = () => {};
 
@@ -7,19 +9,41 @@ export function buildStatusFilters() {
         return statusFilterEls.filter(el => el.checked).map(el => el.value);
     }
 
+    function updateStatusFilterCount() {
+        const selectionCount = document.querySelectorAll('#statusFilters input:checked').length;
+        elStatusFilterCount.textContent = '' + selectionCount;
+    }
+
+    function collapseStatusFilterSection(isCollapsed) {
+        if (isCollapsed) {
+            elViewStatusFilters.removeAttribute('open');
+        } else {
+            elViewStatusFilters.setAttribute('open', 'open');
+        }
+    }
+
     return {
         initialise() {
             statusFilterEls.forEach(elCheckbox => {
                 elCheckbox.addEventListener('change', () => onFiltersChangedHandler());
             });
+
+            const anyStatusesSelected = statusFilterEls.some(el => el.checked);
+            collapseStatusFilterSection(!anyStatusesSelected);
+            updateStatusFilterCount();
         },
         onChange(handler) {
-            onFiltersChangedHandler = handler;
+            onFiltersChangedHandler = () => {
+                updateStatusFilterCount();
+                handler();
+            };
         },
         clear() {
             statusFilterEls.forEach(el => {
                 el.checked = false;
             });
+            collapseStatusFilterSection(true);
+            updateStatusFilterCount();
         },
         buildFilterFunction() {
             const selectedStatuses = new Set(getSelected());
