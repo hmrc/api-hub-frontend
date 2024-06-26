7b602700cfe8243772015ec6c3a13ec052df1c03
PaulCDurham <23658502+PaulCDurham@users.noreply.github.com>
2024-06-26 15:55:56 +0100

    Controller done

---
+++ app/controllers/application/RemoveApiController.scala
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.application
+
+import com.google.inject.{Inject, Singleton}
+import controllers.actions.{ApplicationAuthActionProvider, IdentifierAction}
+import controllers.helpers.ErrorResultBuilder
+import forms.YesNoFormProvider
+import models.application.ApplicationLenses._
+import models.requests.ApplicationRequest
+import play.api.data.Form
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Result}
+import services.ApiHubService
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.application.{RemoveApiConfirmationView, RemoveApiSuccessView}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class RemoveApiController @Inject()(
+  val controllerComponents: MessagesControllerComponents,
+  identify: IdentifierAction,
+  applicationAuth: ApplicationAuthActionProvider,
+  apiHubService: ApiHubService,
+  errorResultBuilder: ErrorResultBuilder,
+  formProvider: YesNoFormProvider,
+  confirmationView: RemoveApiConfirmationView,
+  successView: RemoveApiSuccessView
+)(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  private val form = formProvider("removeApiConfirmation.error")
+
+  def onPageLoad(applicationId: String, apiId: String): Action[AnyContent] = (identify andThen applicationAuth(applicationId)).async {
+    implicit request =>
+      showConfirmationView(Ok, apiId, form)
+  }
+
+  def onSubmit(applicationId: String, apiId: String): Action[AnyContent] = (identify andThen applicationAuth(applicationId)).async {
+    implicit request =>
+      form.bindFromRequest().fold(
+        formWithErrors =>
+          showConfirmationView(BadRequest, apiId, formWithErrors),
+        value =>
+          if (value) {
+            apiHubService.removeApi(applicationId, apiId).map {
+              case Some(_) => Ok(successView(request.application, request.identifierRequest.user))
+              case None => errorResultBuilder.apiNotFoundInApplication(apiId, request.application)
+            }
+          }
+          else {
+            Future.successful(Redirect(controllers.application.routes.ApplicationDetailsController.onPageLoad(applicationId)))
+          }
+      )
+  }
+
+  private def showConfirmationView(status: Status, apiId: String, form: Form[_])(implicit request: ApplicationRequest[_]): Future[Result] = {
+    apiHubService.getApiDetail(apiId).map {
+      case Some(apiDetail) if request.application.hasApi(apiDetail.id) =>
+        status(confirmationView(request.application, apiDetail, form, request.identifierRequest.user))
+      case Some(apiDetail) =>
+        errorResultBuilder.apiNotFoundInApplication(apiDetail, request.application)
+      case None =>
+        errorResultBuilder.apiNotFound(apiId)
+    }
+  }
+
+}
