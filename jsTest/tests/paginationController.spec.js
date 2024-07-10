c73ddfed7a70fedae38432b9f1237ecc9ae487f8
rdawson-hmrc <161580719+rdawson-hmrc@users.noreply.github.com>
2024-07-10 15:30:28 +0100

    HIPP-1360 - pagination refactor - tests

--- jsTest/tests/paginationController.spec.js
+++ jsTest/tests/paginationController.spec.js
@@ -1,108 +1,135 @@
 import {buildPaginator} from '../../app/assets/javascripts/paginationController.js';
 import {JSDOM} from 'jsdom';
-import {isVisible} from "./testUtils.js";
+import {paginationHelper, paginationContainerHtml, isVisible, arrayFromTo} from "./testUtils.js";
 
 describe('paginationController', () => {
-    let elContainer, paginator, document, paginationData;
+    let paginator, document;
 
     beforeEach(() => {
-        const dom = (new JSDOM('<!DOCTYPE html><div id="pg"></div>'))
+        const dom = (new JSDOM(`<!DOCTYPE html><div id="panels"></div>${paginationContainerHtml}`));
         document = dom.window.document;
-        elContainer = document.getElementById('pg');
-        document.body.appendChild(elContainer);
         globalThis.document = document;
-        paginator = buildPaginator(elContainer, 10);
-        paginator.onPaginationChanged(data => {
-            paginationData = data;
-        });
+        paginator = buildPaginator(10);
     });
 
     function buildItems(count) {
-        return Array.from({length: count}, (o, i) => ({index: i}));
-    }
-    function clickNext(){
-        document.querySelector('.govuk-pagination__next ').click();
+        document.getElementById('panels').innerHTML = Array.from({length: count}, (o, i) => `<div data-index="${i}"></div>`).join('');
+        return [...document.querySelectorAll('#panels div')];
     }
 
     describe("isPaginating", () => {
         it("when less than 1 page of items exist, then the pagination view is not visible", () => {
-            paginator.initialise(buildItems(9));
-            expect(paginationData.isPaginating).toBe(false);
-            expect(isVisible(elContainer)).toBe(false);
+            paginator.render(buildItems(9));
+            expect(paginationHelper.paginationIsAvailable()).toBe(false);
         });
 
         it("when exactly 1 page of items exists, then the pagination view is not visible", () => {
-            paginator.initialise(buildItems(10));
-            expect(paginationData.isPaginating).toBe(false);
-            expect(isVisible(elContainer)).toBe(false);
+            paginator.render(buildItems(10));
+            expect(paginationHelper.paginationIsAvailable()).toBe(false);
         });
 
         it("when 1 more than 1 page of items exists, then the pagination view is not visible", () => {
-            paginator.initialise(buildItems(11));
-            expect(paginationData.isPaginating).toBe(true);
-            expect(isVisible(elContainer)).toBe(true);
+            paginator.render(buildItems(11));
+            expect(paginationHelper.paginationIsAvailable()).toBe(true);
         });
     });
 
     describe("item count", () => {
         it("item counts are correct when there is only one page of results", () => {
-            paginator.initialise(buildItems(7));
-            expect(paginationData.visibleItemCount).toBe(7);
-            expect(paginationData.totalItemCount).toBe(7);
+            paginator.render(buildItems(7));
+            expect(paginationHelper.getShowingCount()).toBe(7);
+            expect(paginationHelper.getTotalCount()).toBe(7);
         });
         it("item counts are correct when there are multiple pages and we navigate through each of them", () => {
-            paginator.initialise(buildItems(32));
+            paginator.render(buildItems(32));
 
-            expect(paginationData.visibleItemCount).toBe(10);
-            expect(paginationData.totalItemCount).toBe(32);
+            expect(paginationHelper.getShowingCount()).toBe(10);
+            expect(paginationHelper.getTotalCount()).toBe(32);
 
-            clickNext();
-            expect(paginationData.visibleItemCount).toBe(10);
-            expect(paginationData.totalItemCount).toBe(32);
+            paginationHelper.clickNext();
+            expect(paginationHelper.getShowingCount()).toBe(10);
+            expect(paginationHelper.getTotalCount()).toBe(32);
 
-            clickNext();
-            expect(paginationData.visibleItemCount).toBe(10);
-            expect(paginationData.totalItemCount).toBe(32);
+            paginationHelper.clickNext();
+            expect(paginationHelper.getShowingCount()).toBe(10);
+            expect(paginationHelper.getTotalCount()).toBe(32);
 
-            clickNext();
-            expect(paginationData.visibleItemCount).toBe(2); // final page, this count is different
-            expect(paginationData.totalItemCount).toBe(32);
+            paginationHelper.clickNext();
+            expect(paginationHelper.getShowingCount()).toBe(2); // final page, this count is different
+            expect(paginationHelper.getTotalCount()).toBe(32);
         });
     });
 
-    describe('hiddenByPagination property', () => {
-        function getHiddenItems(items) {
-            return items.filter(item => item.hiddenByPagination);
-        }
-        function buildHiddenItems(startIndex, endIndex) {
-            return Array.from({length: endIndex - startIndex + 1}, (o, i) => ({index: i + startIndex, hiddenByPagination: true}));
-        }
-
-        it("when there is only 1 page of results then no items are marked as hidden", () => {
+    describe('item visibility', () => {
+        it("when there is only 1 page of results then all items are visible", () => {
             const items = buildItems(10);
-            paginator.initialise(items);
-            expect(getHiddenItems(items)).toEqual([]);
+            paginator.render(items);
+            expect(paginationHelper.getVisiblePanelIndexes('#panels div')).toEqual(arrayFromTo(0, 9));
         });
 
         it("when there are multiple pages of results and we are on the first page the correct items are marked as hidden", () => {
             const items = buildItems(24);
-            paginator.initialise(items);
-            expect(getHiddenItems(items)).toEqual(buildHiddenItems(10, 23));
+            paginator.render(items);
+            expect(paginationHelper.getVisiblePanelIndexes('#panels div')).toEqual(arrayFromTo(0, 9));
         });
 
         it("when there are multiple pages of results and we are on the second page the correct items are marked as hidden", () => {
             const items = buildItems(24);
-            paginator.initialise(items);
-            clickNext();
-            expect(getHiddenItems(items)).toEqual([...buildHiddenItems(0, 9), ...buildHiddenItems(20, 23)]);
+            paginator.render(items);
+            paginationHelper.clickNext();
+            expect(paginationHelper.getVisiblePanelIndexes('#panels div')).toEqual(arrayFromTo(10, 19));
         });
 
         it("when there are multiple pages of results and we are on the last page the correct items are marked as hidden", () => {
             const items = buildItems(24);
-            paginator.initialise(items);
-            clickNext();
-            clickNext();
-            expect(getHiddenItems(items)).toEqual(buildHiddenItems(0, 19));
+            paginator.render(items);
+            paginationHelper.clickNext();
+            paginationHelper.clickNext();
+            expect(paginationHelper.getVisiblePanelIndexes('#panels div')).toEqual(arrayFromTo(20, 23));
+        });
+    });
+
+    describe('navigation', () => {
+        it("clicking the next link navigates to the next page", () => {
+            const items = buildItems(30);
+            paginator.render(items);
+
+            expect(paginationHelper.getCurrentPageNumber()).toBe(1);
+
+            paginationHelper.clickNext();
+            expect(paginationHelper.getCurrentPageNumber()).toBe(2);
+
+            paginationHelper.clickNext();
+            expect(paginationHelper.getCurrentPageNumber()).toBe(3);
+        });
+
+        it("clicking the previous link navigates to the previous page", () => {
+            const items = buildItems(30);
+            paginator.render(items);
+            paginationHelper.clickNext();
+            paginationHelper.clickNext();
+
+            expect(paginationHelper.getCurrentPageNumber()).toBe(3);
+
+            paginationHelper.clickPrevious();
+            expect(paginationHelper.getCurrentPageNumber()).toBe(2);
+
+            paginationHelper.clickPrevious();
+            expect(paginationHelper.getCurrentPageNumber()).toBe(1);
+        });
+
+        it("clicking page number links navigates to the correct page", () => {
+            const items = buildItems(30);
+            paginator.render(items);
+
+            paginationHelper.getPaginationPageLink(2).click();
+            expect(paginationHelper.getCurrentPageNumber()).toBe(2);
+
+            paginationHelper.getPaginationPageLink(3).click();
+            expect(paginationHelper.getCurrentPageNumber()).toBe(3);
+
+            paginationHelper.getPaginationPageLink(1).click();
+            expect(paginationHelper.getCurrentPageNumber()).toBe(1);
         });
     });
 })
