3501468c106eed540ae1b0e08214ad3c80b173bb
rdawson-hmrc <161580719+rdawson-hmrc@users.noreply.github.com>
2024-07-18 11:51:51 +0100

    HIPP-1307 - pagination

---
+++ app/assets/javascripts/apiUsage.js
@@ -0,0 +1,12 @@
+import {buildPaginator} from './paginationController.js';
+
+export function onDomLoaded() {
+    const applicationPanelEls = Array.from(document.querySelectorAll('#applicationsTable .govuk-table__body .govuk-table__row')),
+        paginator = buildPaginator(10);
+
+    paginator.render(applicationPanelEls);
+}
+
+if (typeof window !== 'undefined') {
+    window.addEventListener("DOMContentLoaded", onDomLoaded);
+}
