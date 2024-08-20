4556e79c731602cbf88a45457e449dd71720bb4b
victorarbuesmallada <painyjames@gmail.com>
2024-08-20 17:23:26 +0100

    HIPP-1459: Redirect api-hub requests to integration-hub

---
+++ app/routes/OldRoutes.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package routes
+
+import play.api.mvc._
+import play.api.routing.Router.Routes
+import play.api.routing.SimpleRouter
+import play.api.routing.sird._
+
+import javax.inject.Inject
+
+class OldRoutes @Inject()(
+                           val cc: MessagesControllerComponents
+                         ) extends SimpleRouter {
+  private val oldPath = "/api-hub"
+  private val newPath = "/integration-hub"
+
+  private object RedirectController extends ControllerHelpers {
+    private val Action = new ActionBuilder.IgnoringBody()(controllers.Execution.trampoline)
+
+    def redirect(to: String): Action[AnyContent] = Action {
+      Redirect(s"$newPath$to", statusCode = MOVED_PERMANENTLY)
+    }
+  }
+
+  override def routes: Routes = {
+    case GET(path) =>
+      RedirectController.redirect(path.path.replace(oldPath, ""))
+  }
+}
+
