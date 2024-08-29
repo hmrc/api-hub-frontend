8623df7ea64637930e84d3997ef9ac6ee649f551
rdawson-hmrc <161580719+rdawson-hmrc@users.noreply.github.com>
2024-08-29 09:53:46 +0100

    HIPP-1468 - wip

---
+++ app/controllers/application/ApplicationAccessRequestsController.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.application
+
+import com.google.inject.{Inject, Singleton}
+import controllers.actions.{ApplicationAuthActionProvider, AuthorisedSupportAction, IdentifierAction}
+import models.accessrequest.AccessRequest
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import services.ApiHubService
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.application.ApplicationAccessRequestsView
+
+import scala.concurrent.ExecutionContext
+import scala.math.Ordered.orderingToOrdered
+
+@Singleton
+class ApplicationAccessRequestsController @Inject()(
+  override val controllerComponents: MessagesControllerComponents,
+  identify: IdentifierAction,
+  isSupport: AuthorisedSupportAction,
+  applicationAuth: ApplicationAuthActionProvider,
+  apiHubService: ApiHubService,
+  accessRequestsView: ApplicationAccessRequestsView
+)(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  private implicit val accessRequestOrdering: Ordering[AccessRequest] = (x: AccessRequest, y: AccessRequest) => {
+    (y.requested, x.applicationId, x.apiName) compare (x.requested, y.applicationId, y.apiName)
+  }
+
+  def onPageLoad(id: String): Action[AnyContent] = (identify andThen isSupport andThen applicationAuth(id)).async {
+    implicit request =>
+      apiHubService.getAccessRequests(Some(id), None).map {
+        requests =>
+          Ok(accessRequestsView(request.application, requests.sorted, request.identifierRequest.user))
+      }
+  }
+
+}
