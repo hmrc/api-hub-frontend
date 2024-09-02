5999430e7f9c14c9e156859249398638311f1731
Rob Dawson <161580719+rdawson-hmrc@users.noreply.github.com>
2024-09-02 13:39:03 +0100

    HIPP-1401 - filter manage apis by publisher ref (#333)

--- jsTest/tests/adminManageApis.spec.js
+++ jsTest/tests/adminManageApis.spec.js
@@ -1,6 +1,6 @@
 import {JSDOM} from 'jsdom';
 import {onPageShow} from '../../app/assets/javascripts/adminManageApis.js';
-import {paginationHelper, paginationContainerHtml, arrayFromTo} from "./testUtils.js";
+import {paginationHelper, paginationContainerHtml, arrayFromTo, isVisible} from "./testUtils.js";
 
 describe('adminManageApis', () => {
     let document;
@@ -11,7 +11,7 @@ describe('adminManageApis', () => {
             <div id="apiDetailPanels">
                 <div class="hip-api"></div>
             </div>
-            <input id="nameFilter" type="text">
+            <input id="apiFilter" type="text">
             <div id="noResultsPanel"></div>
             <div id="apiCount"></div>
             ${paginationContainerHtml}
@@ -24,13 +24,17 @@ describe('adminManageApis', () => {
     function buildApiPanels(count) {
         document.getElementById('apiDetailPanels').innerHTML = Array.from(
             {length: count},
-            (_, i) => `<div class="hip-api" data-index="${i+1}" data-apiname="Api ${i+1}">Api ${i+1}</div>`
+            (_, i) => `<div class="hip-api" data-index="${i+1}" data-apiname="Api ${i+1}" data-apiref="Ref ${i+1}">Api ${i+1}</div>`
         ).join('');
     }
 
-    function enterNameFilterText(value) {
-        document.getElementById('nameFilter').value = value;
-        document.getElementById('nameFilter').dispatchEvent(new Event('input'));
+    function enterFilterText(value) {
+        document.getElementById('apiFilter').value = value;
+        document.getElementById('apiFilter').dispatchEvent(new Event('input'));
+    }
+
+    function noResultsPanelIsVisible() {
+        return isVisible(document.getElementById('noResultsPanel'));
     }
 
     it("if 10 apis are present on the page then all are visible and pagination is not available",  () => {
@@ -39,6 +43,7 @@ describe('adminManageApis', () => {
         onPageShow();
 
         expect(paginationHelper.paginationIsAvailable()).toBeFalse();
+        expect(noResultsPanelIsVisible()).toBeFalse();
     });
 
     it("if 11 apis are present on the page then only the first 10 are visible and pagination is available",  () => {
@@ -48,13 +53,14 @@ describe('adminManageApis', () => {
 
         expect(paginationHelper.paginationIsAvailable()).toBeTrue();
         expect(paginationHelper.getVisiblePanelIndexes('.hip-api')).toEqual(arrayFromTo(1, 10));
+        expect(noResultsPanelIsVisible()).toBeFalse();
     });
 
-    it("when the user enters some filter text then only the APIs that match the filter are shown",  () => {
+    it("when the user enters some filter text then APIs with names that match the filter are shown",  () => {
         buildApiPanels(100);
 
         onPageShow();
-        enterNameFilterText('Api 1');
+        enterFilterText('Api 1');
 
         expect(paginationHelper.getVisiblePanelData('.hip-api', 'apiname').map(o => o.apiname)).toEqual([
             'Api 1', 'Api 10', 'Api 11', 'Api 12', 'Api 13', 'Api 14', 'Api 15', 'Api 16', 'Api 17', 'Api 18'
@@ -63,6 +69,33 @@ describe('adminManageApis', () => {
 
         paginationHelper.clickNext();
         expect(paginationHelper.getVisiblePanelData('.hip-api', 'apiname').map(o => o.apiname)).toEqual(['Api 19', 'Api 100']);
+        expect(noResultsPanelIsVisible()).toBeFalse();
     });
 
+    it("when the user enters some filter text then APIs with publisher-refs that match the filter are shown",  () => {
+        buildApiPanels(100);
+
+        onPageShow();
+        enterFilterText('Ref 1');
+
+        expect(paginationHelper.getVisiblePanelData('.hip-api', 'apiref').map(o => o.apiref)).toEqual([
+            'Ref 1', 'Ref 10', 'Ref 11', 'Ref 12', 'Ref 13', 'Ref 14', 'Ref 15', 'Ref 16', 'Ref 17', 'Ref 18'
+        ]);
+        expect(paginationHelper.paginationIsAvailable()).toBeTrue();
+
+        paginationHelper.clickNext();
+        expect(paginationHelper.getVisiblePanelData('.hip-api', 'apiref').map(o => o.apiref)).toEqual(['Ref 19', 'Ref 100']);
+        expect(noResultsPanelIsVisible()).toBeFalse();
+    });
+
+    it("when the user enters some filter text that does not match any APIs then no APIs are shown",  () => {
+        buildApiPanels(100);
+
+        onPageShow();
+        enterFilterText('nomathces');
+
+        expect(paginationHelper.getVisiblePanelIndexes()).toEqual([]);
+        expect(paginationHelper.paginationIsAvailable()).toBeFalse();
+        expect(noResultsPanelIsVisible()).toBeTrue();
+    });
 });
