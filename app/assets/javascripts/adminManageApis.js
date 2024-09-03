ab1f7990b9f059c12b64cee65b8fddad2005ed0b
rdawson-hmrc <161580719+rdawson-hmrc@users.noreply.github.com>
2024-09-03 15:39:38 +0100

    HIPP-1483 - refactoring adminManageApis

--- app/assets/javascripts/adminManageApis.js
+++ app/assets/javascripts/adminManageApis.js
@@ -1,33 +1,20 @@
 import {buildPaginator} from './paginationController.js';
-import {noop, normaliseText, setVisible} from "./utils.js";
+import {setVisible} from "./utils.js";
+import {buildTextFilter, dataAttribute} from "./textFilter.js";
 
 export function onPageShow() {
     const view = (() => {
-        const apiDetailPanelEls = Array.from(document.querySelectorAll('#apiDetailPanels .hip-api')),
+        const apiDetailPanelEls = document.querySelectorAll('#apiDetailPanels .hip-api'),
             elNoResultsPanel = document.getElementById('noResultsPanel'),
             elApiCount = document.getElementById('apiCount'),
             elApiFilter = document.getElementById('apiFilter');
-
-        let onFiltersChangedHandler = noop;
-
-        elApiFilter.addEventListener('input', () => {
-            onFiltersChangedHandler();
-        });
 
         return {
             get apiDetailPanels() {
                 return [...apiDetailPanelEls];
             },
-            onFiltersChanged(handler) {
-                onFiltersChangedHandler = handler;
-            },
-            setApiPanelVisibility(apis) {
-                apis.forEach(apiDetail => {
-                    setVisible(apiDetail.el, !apiDetail.hiddenByFilter);
-                });
-            },
-            get apiFilterValue() {
-                return elApiFilter.value;
+            get apiFilter() {
+                return elApiFilter;
             },
             toggleNoResultsPanel(visible) {
                 setVisible(elNoResultsPanel, visible);
@@ -38,39 +25,21 @@ export function onPageShow() {
         };
     })();
 
-    const apiPanels = view.apiDetailPanels.map(el => ({
-        el,
-        apiName: el.dataset['apiname'],
-        apiRef: el.dataset['apiref'],
-        hiddenByFilter: false
-    }));
+    const paginator = buildPaginator(10),
+        filter = buildTextFilter(
+            view.apiDetailPanels,
+            view.apiFilter,
+            [
+                dataAttribute('apiname').whenNormalised().includesTheFilterText(),
+                dataAttribute('apiref').whenNormalised().includesTheFilterText(),
+            ]
+        );
 
-    const paginator = buildPaginator(10);
-
-    function applyFilter() {
-        const normalisedFilterValue = normaliseText(view.apiFilterValue);
-        apiPanels.forEach(apiDetail => {
-            const filterMatchesName = normaliseText(apiDetail.apiName).includes(normalisedFilterValue),
-                filterMatchesRef = normaliseText(apiDetail.apiRef).includes(normalisedFilterValue),
-                filterMatchesAnything = filterMatchesName || filterMatchesRef;
-            apiDetail.hiddenByFilter = ! filterMatchesAnything;
-        });
-
-        const filteredPanels = apiPanels.filter(apiDetail => ! apiDetail.hiddenByFilter);
-        view.setApiPanelVisibility(apiPanels);
-        paginator.render(filteredPanels.map(panel => panel.el));
-
-        const resultCount = filteredPanels.length;
-        view.toggleNoResultsPanel(resultCount === 0);
-        view.resultCount = resultCount;
-    }
-
-    view.onFiltersChanged(() => {
-        applyFilter();
+    filter.onChange(matchingEls => {
+        view.resultCount = matchingEls.length;
+        view.toggleNoResultsPanel(matchingEls.length === 0);
+        paginator.render(matchingEls);
     });
-
-    paginator.render(apiPanels.map(o => o.el));
-    applyFilter();
 }
 
 if (typeof window !== 'undefined') {
