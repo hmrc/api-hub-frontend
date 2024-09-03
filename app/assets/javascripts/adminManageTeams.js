22f4160817d75f9e163664680591109c6938132b
rdawson-hmrc <161580719+rdawson-hmrc@users.noreply.github.com>
2024-09-03 13:23:24 +0100

    HIPP-1481 - wip

--- app/assets/javascripts/adminManageTeams.js
+++ app/assets/javascripts/adminManageTeams.js
@@ -1,10 +1,46 @@
 import {buildPaginator} from './paginationController.js';
+import {noop, normaliseText, setVisible} from "./utils.js";
+
+function buildFilter(itemEls, elFilterInput, searchableAttributes) {
+    let onFiltersChangedHandler = noop;
+
+    elFilterInput.addEventListener('input', () => {
+        const matchingEls = applyFilter();
+        onFiltersChangedHandler(matchingEls);
+    });
+
+    const itemModels =  [...itemEls].map(el => {
+        const itemModel = {el, hiddenByFilter: false};
+        searchableAttributes.forEach(attr => {
+           itemModel[attr] = el.dataset[attr];
+        });
+        return itemModel;
+    });
+
+    function applyFilter() {
+        const normalisedFilterValue = normaliseText(elFilterInput.value);
+        itemModels.forEach(itemModel => {
+            itemModel.hiddenByFilter = ! searchableAttributes.some(attr => normaliseText(itemModel[attr]).includes(normalisedFilterValue));
+            setVisible(itemModel.el, ! itemModel.hiddenByFilter);
+        });
+
+        return itemModels.filter(apiDetail => ! apiDetail.hiddenByFilter);
+    }
+
+    return {
+        onChange: onFiltersChangedHandler
+    };
+}
 
 export function onDomLoaded() {
-    const teamRowEls = Array.from(document.querySelectorAll('#teamsTable .govuk-table__row')),
-        paginator = buildPaginator(10);
+    const paginator = buildPaginator(10),
+        filter = buildFilter(
+            document.querySelectorAll('#teamsTable .govuk-table__body .govuk-table__row'),
+            document.getElementById('teamFilter'),
+            ['name', 'id', 'emails']
+        );
 
-    paginator.render(teamRowEls);
+    filter.onChange(paginator.render);
 }
 
 if (typeof window !== 'undefined') {
