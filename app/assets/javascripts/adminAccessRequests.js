1b6a61ddd2b37fb456e5ed2b7718cda7df864299
rdawson-hmrc <161580719+rdawson-hmrc@users.noreply.github.com>
2024-09-25 11:54:58 +0100

    HIPP-1555 - js and unit tests, fixes

--- app/assets/javascripts/adminAccessRequests.js
+++ app/assets/javascripts/adminAccessRequests.js
@@ -1,11 +1,33 @@
 import {buildPaginator} from './paginationController.js';
+import {setVisible} from "./utils.js";
 
-export function onDomLoaded() {
-    const accessRequestPanelEls = Array.from(document.querySelectorAll('#accessRequestList .hip-access-request-panel')),
-        paginator = buildPaginator(10);
-    paginator.render(accessRequestPanelEls);
+export function onPageShow() {
+    const filterByStatusCheckboxEls = Array.from(document.querySelectorAll('#filterByStatusCheckboxes input[type="checkbox"]')),
+        accessRequestPanelEls = Array.from(document.querySelectorAll('#accessRequestList .hip-access-request-panel')),
+        elRequestCount = document.getElementById('requestCount'),
+        paginator = buildPaginator(10),
+        requestsModel = accessRequestPanelEls.map(el => ({
+            el,
+            status: el.dataset.status,
+            hiddenByFilters: false
+        }));
+
+    function onFilterUpdate() {
+        const selectedStatuses = new Set(filterByStatusCheckboxEls.filter(checkbox => checkbox.checked).map(checkbox => checkbox.value));
+        requestsModel.forEach(accessRequest => {
+            accessRequest.hiddenByFilters = selectedStatuses.size > 0 && !selectedStatuses.has(accessRequest.status);
+            setVisible(accessRequest.el, !accessRequest.hiddenByFilters);
+        });
+
+        const unfilteredRequests = requestsModel.filter(accessRequest => !accessRequest.hiddenByFilters);
+        elRequestCount.textContent = unfilteredRequests.length.toString();
+        paginator.render(unfilteredRequests.map(accessRequest => accessRequest.el));
+    }
+    filterByStatusCheckboxEls.forEach(checkbox => checkbox.addEventListener('change', onFilterUpdate));
+
+    onFilterUpdate();
 }
 
 if (typeof window !== 'undefined') {
-    window.addEventListener("DOMContentLoaded", onDomLoaded);
+    window.addEventListener("pageshow", onPageShow);
 }
