104c9253c851fc86b9269deaa59845a0a8a77222
PaulCDurham <23658502+PaulCDurham@users.noreply.github.com>
2024-10-01 15:27:18 +0100

    View done

--- test/controllers/application/cancelaccessrequest/CancelAccessRequestSelectApiControllerSpec.scala
+++ test/controllers/application/cancelaccessrequest/CancelAccessRequestSelectApiControllerSpec.scala
@@ -19,144 +19,204 @@ package controllers.application.cancelaccessrequest
 import base.SpecBase
 import controllers.actions.FakeUser
 import forms.application.cancelaccessrequest.CancelAccessRequestSelectApiFormProvider
-import models.accessrequest.AccessRequest
+import generators.AccessRequestGenerator
 import models.{NormalMode, UserAnswers}
+import models.accessrequest.AccessRequest
 import navigation.{FakeNavigator, Navigator}
+import org.mockito.ArgumentMatchers
 import org.mockito.ArgumentMatchers.any
-import org.mockito.Mockito.when
+import org.mockito.Mockito.{verify, when}
 import org.scalatestplus.mockito.MockitoSugar
-import pages.application.cancelaccessrequest.CancelAccessRequestSelectApiPage
+import pages.application.cancelaccessrequest.{CancelAccessRequestPendingPage, CancelAccessRequestSelectApiPage}
+import play.api.Application
+import play.api.data.Form
 import play.api.inject.bind
 import play.api.mvc.Call
 import play.api.test.FakeRequest
 import play.api.test.Helpers.*
-import repositories.SessionRepository
+import repositories.CancelAccessRequestSessionRepository
+import utils.{HtmlValidation, UserAnswersSugar}
 import views.html.application.cancelaccessrequest.CancelAccessRequestSelectApiView
 
 import scala.concurrent.Future
 
-class CancelAccessRequestSelectApiControllerSpec extends SpecBase with MockitoSugar {
+class CancelAccessRequestSelectApiControllerSpec extends SpecBase with MockitoSugar with HtmlValidation with UserAnswersSugar with AccessRequestGenerator {
 
-  def onwardRoute = Call("GET", "/foo")
-
-  lazy val cancelAccessRequestSelectApiRoute = routes.CancelAccessRequestSelectApiController.onPageLoad(NormalMode).url
-
-  val accessRequests: Seq[AccessRequest] = Seq.empty
-  val formProvider = new CancelAccessRequestSelectApiFormProvider()
-  val form = formProvider(accessRequests)
+  import CancelAccessRequestSelectApiControllerSpec.*
 
   "CancelAccessRequestSelectApi Controller" - {
 
     "must return OK and the correct view for a GET" in {
+      val accessRequests = sampleAccessRequests()
+      val fixture = buildFixture(accessRequests = Some(accessRequests))
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
-
-      running(application) {
+      running(fixture.application) {
         val request = FakeRequest(GET, cancelAccessRequestSelectApiRoute)
+        val result = route(fixture.application, request).value
 
-        val result = route(application, request).value
+        status(result) mustBe OK
 
-        val view = application.injector.instanceOf[CancelAccessRequestSelectApiView]
-
-        status(result) mustEqual OK
-
-        contentAsString(result) mustEqual view(accessRequests, form, NormalMode, FakeUser)(request, messages(application)).toString
+        contentAsString(result) mustBe fixture.view(accessRequests, fixture.form, NormalMode, FakeUser)(request, messages(fixture.application)).toString
+        contentAsString(result) must validateAsHtml
       }
     }
 
     "must populate the view correctly on a GET when the question has previously been answered" in {
-      val selectedApis = Set("etc")
-      val userAnswers = UserAnswers(userAnswersId).set(CancelAccessRequestSelectApiPage, selectedApis).success.value
+      val accessRequests = sampleAccessRequests()
+      val selectedApis = Set(accessRequests.head.id)
+      val fixture = buildFixture(accessRequests = Some(accessRequests), selectedApis = Some(selectedApis))
 
-      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
-
-      running(application) {
+      running(fixture.application) {
         val request = FakeRequest(GET, cancelAccessRequestSelectApiRoute)
+        val result = route(fixture.application, request).value
+        val filledForm = fixture.form.fill(selectedApis)
 
-        val view = application.injector.instanceOf[CancelAccessRequestSelectApiView]
-
-        val result = route(application, request).value
-
-        status(result) mustEqual OK
-        contentAsString(result) mustEqual view(accessRequests, form.fill(selectedApis), NormalMode, FakeUser)(request, messages(application)).toString
+        status(result) mustBe OK
+        contentAsString(result) mustBe fixture.view(accessRequests, filledForm, NormalMode, FakeUser)(request, messages(fixture.application)).toString
+        contentAsString(result) must validateAsHtml
       }
     }
 
     "must redirect to the next page when valid data is submitted" in {
-      val selectedApis = Set("etc")
-      val mockSessionRepository = mock[SessionRepository]
+      val accessRequests = sampleAccessRequests()
+      val fixture = buildFixture(accessRequests = Some(accessRequests))
 
-      when(mockSessionRepository.set(any())).thenReturn(Future.successful(true))
+      when(fixture.sessionRepository.set(any())).thenReturn(Future.successful(true))
 
-      val application =
-        applicationBuilder(userAnswers = Some(emptyUserAnswers))
-          .overrides(
-            bind[Navigator].toInstance(new FakeNavigator(onwardRoute)),
-            bind[SessionRepository].toInstance(mockSessionRepository)
-          )
-          .build()
+      running(fixture.application) {
+        val request = FakeRequest(POST, cancelAccessRequestSelectApiRoute)
+          .withFormUrlEncodedBody(("value[0]", accessRequests.head.id))
 
-      running(application) {
-        val request =
-          FakeRequest(POST, cancelAccessRequestSelectApiRoute)
-            .withFormUrlEncodedBody(("value[0]", selectedApis.head))
+        val result = route(fixture.application, request).value
 
-        val result = route(application, request).value
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result).value mustBe onwardRoute.url
 
-        status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual onwardRoute.url
+        val expectedAnswers = fixture.userAnswers.value.setQuick(CancelAccessRequestSelectApiPage, Set(accessRequests.head.id))
+        verify(fixture.sessionRepository).set(ArgumentMatchers.eq(expectedAnswers))
       }
     }
 
     "must return a Bad Request and errors when invalid data is submitted" in {
+      val accessRequests = sampleAccessRequests()
+      val fixture = buildFixture(accessRequests = Some(accessRequests))
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      running(fixture.application) {
+        val request = FakeRequest(POST, cancelAccessRequestSelectApiRoute)
+          .withFormUrlEncodedBody(("value", "invalid value"))
 
-      running(application) {
-        val request =
-          FakeRequest(POST, cancelAccessRequestSelectApiRoute)
-            .withFormUrlEncodedBody(("value", "invalid value"))
+        val boundForm = fixture.form.bind(Map("value" -> "invalid value"))
+        val result = route(fixture.application, request).value
 
-        val boundForm = form.bind(Map("value" -> "invalid value"))
-
-        val view = application.injector.instanceOf[CancelAccessRequestSelectApiView]
-
-        val result = route(application, request).value
-
-        status(result) mustEqual BAD_REQUEST
-        contentAsString(result) mustEqual view(accessRequests, boundForm, NormalMode, FakeUser)(request, messages(application)).toString
+        status(result) mustBe BAD_REQUEST
+        contentAsString(result) mustBe fixture.view(accessRequests, boundForm, NormalMode, FakeUser)(request, messages(fixture.application)).toString
+        contentAsString(result) must validateAsHtml
       }
     }
 
     "must redirect to Journey Recovery for a GET if no existing data is found" in {
+      val fixture = buildFixture(hasAnswers = false)
 
-      val application = applicationBuilder(userAnswers = None).build()
-
-      running(application) {
+      running(fixture.application) {
         val request = FakeRequest(GET, cancelAccessRequestSelectApiRoute)
 
-        val result = route(application, request).value
+        val result = route(fixture.application, request).value
 
-        status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual controllers.routes.JourneyRecoveryController.onPageLoad().url
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result).value mustBe controllers.routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if data is found but has no CancelAccessRequestPendingPage answer" in {
+      val fixture = buildFixture()
+
+      running(fixture.application) {
+        val request = FakeRequest(GET, cancelAccessRequestSelectApiRoute)
+
+        val result = route(fixture.application, request).value
+
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result).value mustBe controllers.routes.JourneyRecoveryController.onPageLoad().url
       }
     }
 
     "must redirect to Journey Recovery for a POST if no existing data is found" in {
+      val fixture = buildFixture(hasAnswers = false)
 
-      val application = applicationBuilder(userAnswers = None).build()
-
-      running(application) {
+      running(fixture.application) {
         val request =
           FakeRequest(POST, cancelAccessRequestSelectApiRoute)
             .withFormUrlEncodedBody(("value[0]", "etc"))
 
-        val result = route(application, request).value
+        val result = route(fixture.application, request).value
 
-        status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual controllers.routes.JourneyRecoveryController.onPageLoad().url
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result).value mustBe controllers.routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must redirect to Journey Recovery for a POST if data is found but has no CancelAccessRequestPendingPage answer" in {
+      val fixture = buildFixture()
+
+      running(fixture.application) {
+        val request =
+          FakeRequest(POST, cancelAccessRequestSelectApiRoute)
+            .withFormUrlEncodedBody(("value[0]", "etc"))
+
+        val result = route(fixture.application, request).value
+
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result).value mustBe controllers.routes.JourneyRecoveryController.onPageLoad().url
       }
     }
   }
+
+  private def buildFixture(
+    hasAnswers: Boolean = true,
+    accessRequests: Option[Seq[AccessRequest]] = None,
+    selectedApis: Option[Set[String]] = None
+  ): Fixture = {
+    val userAnswers = if (hasAnswers) {
+      Some(
+        emptyUserAnswers
+          .setIfPresent(CancelAccessRequestPendingPage, accessRequests)
+          .setIfPresent(CancelAccessRequestSelectApiPage, selectedApis)
+      )
+    }
+    else {
+      None
+    }
+
+    val sessionRepository = mock[CancelAccessRequestSessionRepository]
+
+    val application = applicationBuilder(userAnswers = userAnswers)
+      .overrides(
+        bind[CancelAccessRequestSessionRepository].toInstance(sessionRepository),
+        bind[Navigator].toInstance(FakeNavigator(onwardRoute))
+      )
+      .build()
+
+    val formProvider = new CancelAccessRequestSelectApiFormProvider()
+    val form = formProvider(accessRequests.getOrElse(Seq.empty))
+
+    val view = application.injector.instanceOf[CancelAccessRequestSelectApiView]
+
+    Fixture(userAnswers, sessionRepository, form, view, application)
+  }
+
+}
+
+object CancelAccessRequestSelectApiControllerSpec {
+
+  private val onwardRoute = Call("GET", "/CancelAccessRequestSelectApiControllerSpec")
+  private lazy val cancelAccessRequestSelectApiRoute = routes.CancelAccessRequestSelectApiController.onPageLoad(NormalMode).url
+
+  private case class Fixture(
+    userAnswers: Option[UserAnswers],
+    sessionRepository: CancelAccessRequestSessionRepository,
+    form: Form[Set[String]],
+    view: CancelAccessRequestSelectApiView,
+    application: Application
+  )
 
 }
