85f77adbe84acc097fb59f4451ea3b838bf52b88
Mike Jackson <10079986+llmikeyj@users.noreply.github.com>
2024-10-01 10:54:10 +0100

    HIPP-1246: Upgrade dependencies (#376)
    * HIPP-1246: Upgrade dependencies
    
    * HIPP-1246: Upgrade dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,17 +3,17 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  private val bootstrapPlayVersion = "9.3.0"
-  private val hmrcMongoVersion = "2.1.0"
+  private val bootstrapPlayVersion = "9.5.0"
+  private val hmrcMongoVersion = "2.2.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "10.0.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"             % "10.12.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30"  % "3.2.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"             % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "internal-auth-client-play-30"           % "3.0.0",
-    "uk.gov.hmrc"       %% "crypto-json-play-30"                    % "8.0.0",
+    "uk.gov.hmrc"       %% "crypto-json-play-30"                    % "8.1.0",
     "org.commonmark"    %  "commonmark"                             % "0.22.0",
     "org.commonmark"    %  "commonmark-ext-gfm-tables"              % "0.22.0",
     "io.swagger.parser.v3" % "swagger-parser" % "2.1.22" excludeAll(
