195a0e85785900a5d8bbab35987276f547e23868
victorarbuesmallada <painyjames@gmail.com>
2024-10-18 12:56:52 +0100

    Preformat OAS global error content

--- app/viewmodels/govuk/ErrorSummaryFluency.scala
+++ app/viewmodels/govuk/ErrorSummaryFluency.scala
@@ -18,7 +18,7 @@ package viewmodels.govuk
 
 import play.api.data.Form
 import play.api.i18n.Messages
-import uk.gov.hmrc.govukfrontend.views.viewmodels.content.{Content, Text}
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.{Content, HtmlContent, Text}
 import uk.gov.hmrc.govukfrontend.views.viewmodels.errorsummary.{ErrorLink, ErrorSummary}
 
 object errorsummary extends ErrorSummaryFluency
@@ -29,14 +29,19 @@ trait ErrorSummaryFluency {
 
     def apply(
                form: Form[?],
-               errorLinkOverrides: Map[String, String] = Map.empty
+               errorLinkOverrides: Map[String, String] = Map.empty,
+               preFormat: Boolean = false,
              )(implicit messages: Messages): ErrorSummary = {
 
       val errors = form.errors.map {
         error =>
+          val errormessage = messages(error.message, error.args*)
           ErrorLink(
             href    = Some(s"#${errorLinkOverrides.getOrElse(error.key, error.key)}"),
-            content = Text(messages(error.message, error.args*))
+            content = if (preFormat)
+                HtmlContent(s"<pre>$errormessage</pre>")
+              else
+                Text(errormessage)
           )
       }
 
