989e3249c298ae1831230a4148ddc31ec68ea6fa
Victor Arbues <painyjames@gmail.com>
2024-11-28 16:14:04 +0000

    HIPP-1703: Add hip environments config to front end (#467)

---
+++ app/config/HipEnvironments.scala
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package config
+
+import com.google.inject.{Inject, Singleton}
+import com.typesafe.config.Config
+import models.application.EnvironmentName
+import play.api.{ConfigLoader, Configuration}
+
+case class HipEnvironment(
+                           id: String,
+                           rank: Int,                          // 1 is production
+                           nameKey: String,                    // Key for the environment's name in messages file
+                           environmentName: EnvironmentName,   // Needed to pass environment round existing code and in URL paths to backend
+                           isProductionLike: Boolean
+                         )
+
+object HipEnvironment {
+
+  implicit val hipEnvironmentConfigLoader: ConfigLoader[HipEnvironment] =
+    (rootConfig: Config, path: String) => {
+      val config = rootConfig.getConfig(path)
+
+      HipEnvironment(
+        id = config.getString("id"),
+        rank = config.getInt("rank"),
+        nameKey = config.getString("nameKey"),
+        environmentName = EnvironmentName.enumerable.withName(config.getString("environmentName")).getOrElse(throw new IllegalArgumentException()),
+        isProductionLike = config.getBoolean("isProductionLike")
+      )
+    }
+
+}
+
+trait HipEnvironments {
+
+  def environments: Seq[HipEnvironment]
+
+  def forId(id: String): Option[HipEnvironment] = {
+    environments.find(_.id == id)
+  }
+
+}
+
+@Singleton
+class HipEnvironmentsImpl @Inject(configuration: Configuration) extends HipEnvironments {
+
+  override val environments: Seq[HipEnvironment] = {
+    configuration
+      .get[Map[String, HipEnvironment]]("hipEnvironments")
+      .values
+      .toSeq
+      .sortBy(_.rank)
+  }
+
+}
---
+++ test/config/HipEnvironmentsSpec.scala
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package config
+
+import com.typesafe.config.ConfigFactory
+import fakes.FakeEmailDomains
+import models.application.{EnvironmentName, Primary, Secondary}
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
+import play.api.Configuration
+
+class HipEnvironmentsSpec extends AnyFreeSpec with Matchers with TableDrivenPropertyChecks {
+
+  private val hoconConfig =
+    """
+      |hipEnvironments = {
+      |    production = {
+      |        id = "production",
+      |        rank = 1,
+      |        nameKey = "hipEnvironment.production.name",
+      |        environmentName = "primary",
+      |        isProductionLike = true
+      |    },
+      |    test = {
+      |        id = "test",
+      |        rank = 2,
+      |        nameKey = "hipEnvironment.test.name",
+      |        environmentName = "secondary",
+      |        isProductionLike = false
+      |    }
+      |}
+      |""".stripMargin
+  private val hipEnvironmentsConfig = Configuration.apply(ConfigFactory.parseString(hoconConfig))
+  private val expectedHipEnvironments = Seq(
+    HipEnvironment(
+      id = "production",
+      rank = 1,
+      nameKey = "hipEnvironment.production.name",
+      environmentName = Primary,
+      isProductionLike = true
+    ),
+    HipEnvironment(
+      id = "test",
+      rank = 2,
+      nameKey = "hipEnvironment.test.name",
+      environmentName = Secondary,
+      isProductionLike = false
+    ),
+  )
+
+  "HipEnvironments" - {
+    "must load the expected environments" in {
+      val hipEnvironments = HipEnvironmentsImpl(hipEnvironmentsConfig)
+
+      hipEnvironments.environments mustBe expectedHipEnvironments
+    }
+    "must retrieve the expected environments by id" in {
+      val hipEnvironments = HipEnvironmentsImpl(hipEnvironmentsConfig)
+
+      forAll(Table(
+        ("environmentId", "expectedEnvironment"),
+        ("production", expectedHipEnvironments.head),
+        ("test", expectedHipEnvironments.last),
+      )) { (environmentId: String, expectedEnvironment: HipEnvironment) =>
+        hipEnvironments.forId(environmentId) mustBe Some(expectedEnvironment)
+      }
+    }
+  }
+
+}
