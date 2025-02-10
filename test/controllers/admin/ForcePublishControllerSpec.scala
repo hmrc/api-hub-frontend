2dd5d34dd472a65f3bf9f8adb02d8a4b562f246e
PaulCDurham <23658502+PaulCDurham@users.noreply.github.com>
2025-02-10 11:37:50 +0000

    Tests done

--- test/controllers/admin/ForcePublishControllerSpec.scala
+++ test/controllers/admin/ForcePublishControllerSpec.scala
@@ -18,8 +18,13 @@ package controllers.admin
 
 import base.SpecBase
 import controllers.actions.FakeSupporter
+import fakes.FakeHipEnvironments
 import forms.admin.ForcePublishPublisherReferenceFormProvider
+import generators.ApiDetailGenerators
+import models.api.ApiDeploymentStatus.Deployed
 import models.user.UserModel
+import org.mockito.ArgumentMatchers.{any, eq as eqTo}
+import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar
 import play.api.Application
 import play.api.data.Form
@@ -29,13 +34,17 @@ import play.api.test.Helpers.*
 import services.ApiHubService
 import utils.{HtmlValidation, TestHelpers}
 import viewmodels.admin.ForcePublishViewModel
-import views.html.admin.ForcePublishView
+import views.html.ErrorTemplate
+import views.html.admin.{ForcePublishSuccessView, ForcePublishView}
+
+import scala.concurrent.Future
 
 class ForcePublishControllerSpec
   extends SpecBase
     with MockitoSugar
     with TestHelpers
-    with HtmlValidation {
+    with HtmlValidation
+    with ApiDetailGenerators {
 
   import ForcePublishControllerSpec.*
 
@@ -132,11 +141,117 @@ class ForcePublishControllerSpec
   }
 
   "showVersionComparison" - {
+    "must return Ok and the correct view for a support user" in {
+      forAll(usersWhoCanSupport) { (user: UserModel) =>
+        val fixture = buildFixture(user)
+        val environment = FakeHipEnvironments.deploymentHipEnvironment
+        val deploymentStatus = Deployed(environment.id, "test-deployed-version")
+        val apiDetail = sampleApiDetail()
 
+        when(fixture.apiHubService.getApiDeploymentStatus(
+          eqTo(FakeHipEnvironments.deploymentHipEnvironment),
+          eqTo(publisherReference)
+        )(any)).thenReturn(Future.successful(deploymentStatus))
+
+        when(fixture.apiHubService.getApiDetailForPublishReference(eqTo(publisherReference))(any))
+          .thenReturn(Future.successful(Some(apiDetail)))
+
+        running(fixture.application) {
+          val request = FakeRequest(routes.ForcePublishController.showVersionComparison(publisherReference))
+          val result = route(fixture.application, request).value
+          val view = fixture.application.injector.instanceOf[ForcePublishView]
+
+          val viewModel = ForcePublishViewModel(
+            form = form.fill(publisherReference),
+            user = user,
+            publisherReference = Some(publisherReference),
+            deploymentStatus = Some(deploymentStatus),
+            catalogueVersion = Some(apiDetail.version)
+          )
+
+          status(result) mustBe OK
+          contentAsString(result) mustBe view(viewModel)(request, messages(fixture.application)).toString
+          contentAsString(result) must validateAsHtml
+        }
+      }
+    }
+
+    "must return Unauthorized for a non-support user" in {
+      forAll(usersWhoCannotSupport) { (user: UserModel) =>
+        val fixture = buildFixture(user)
+
+        running(fixture.application) {
+          val request = FakeRequest(routes.ForcePublishController.showVersionComparison(publisherReference))
+          val result = route(fixture.application, request).value
+
+          status(result) mustBe SEE_OTHER
+          redirectLocation(result) mustBe Some(controllers.routes.UnauthorisedController.onPageLoad.url)
+        }
+      }
+    }
   }
 
   "forcePublish" - {
+    "must force publish the API and return the success view for a support user" in {
+      forAll(usersWhoCanSupport) { (user: UserModel) =>
+        val fixture = buildFixture(user)
 
+        when(fixture.apiHubService.forcePublish(eqTo(publisherReference))(any))
+          .thenReturn(Future.successful(Some(())))
+
+        running(fixture.application) {
+          val request = FakeRequest(routes.ForcePublishController.forcePublish(publisherReference))
+          val result = route(fixture.application, request).value
+          val view = fixture.application.injector.instanceOf[ForcePublishSuccessView]
+
+          status(result) mustBe OK
+          contentAsString(result) mustBe view(publisherReference, user)(request, messages(fixture.application)).toString
+          contentAsString(result) must validateAsHtml
+        }
+      }
+    }
+
+    "must return 404 Not Found when the deployment is not found in APIM" in {
+      forAll(usersWhoCanSupport) { (user: UserModel) =>
+        val fixture = buildFixture(user)
+
+        when(fixture.apiHubService.forcePublish(eqTo(publisherReference))(any))
+          .thenReturn(Future.successful(None))
+
+        running(fixture.application) {
+          val request = FakeRequest(routes.ForcePublishController.forcePublish(publisherReference))
+          val result = route(fixture.application, request).value
+          val view = fixture.application.injector.instanceOf[ErrorTemplate]
+
+          status(result) mustBe NOT_FOUND
+
+          contentAsString(result) mustBe
+            view(
+              "Page not found - 404",
+              "This deployment was not found in APIM",
+              s"Could not find a deployment or OAS document in APIM for publisher reference $publisherReference.",
+              Some(user)
+            )(request, messages(fixture.application))
+              .toString()
+
+          contentAsString(result) must validateAsHtml
+        }
+      }
+    }
+
+    "must return Unauthorized for a non-support user" in {
+      forAll(usersWhoCannotSupport) { (user: UserModel) =>
+        val fixture = buildFixture(user)
+
+        running(fixture.application) {
+          val request = FakeRequest(routes.ForcePublishController.forcePublish(publisherReference))
+          val result = route(fixture.application, request).value
+
+          status(result) mustBe SEE_OTHER
+          redirectLocation(result) mustBe Some(controllers.routes.UnauthorisedController.onPageLoad.url)
+        }
+      }
+    }
   }
 
   private def buildFixture(user: UserModel = FakeSupporter): Fixture = {
@@ -154,7 +269,7 @@ class ForcePublishControllerSpec
 
 private object ForcePublishControllerSpec {
 
-  val form: Form[?] = new ForcePublishPublisherReferenceFormProvider()()
+  val form: Form[String] = new ForcePublishPublisherReferenceFormProvider()()
   val publisherReference = "test-publisher-reference"
 
   case class Fixture(
